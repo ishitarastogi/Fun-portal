7ff09fd358ebd23a8da4c73e1ace4bf025ce8a86
ishitarastogi <ishita3099@gmail.com>
2021-10-29 12:58:42 +0530

    Add mapping

--- contracts/FunPortal.sol
+++ contracts/FunPortal.sol
@@ -5,7 +5,24 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract FunPortal {
+    uint256 totalFuns;
+    mapping(address=>uint)  funnersCount;
+
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("Yo yo, I am a contract am I am smart");
+    }
+
+    function Fun() public {
+        totalFuns += 1;
+        funnersCount[msg.sender]++;
+        console.log("%s has send some fun stuff!", msg.sender);
+    }
+
+    function getTotalFuns() public view returns (uint256) {
+        console.log("We have %d total funs stuffs!", totalFuns);
+        return totalFuns;
+    }
+    function getFunners(address _funnerAddress) public view returns(uint256){
+        return funnersCount[_funnerAddress];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,22 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+
   const funContractFactory = await hre.ethers.getContractFactory('FunPortal');
   const funContract = await funContractFactory.deploy();
   await funContract.deployed();
   console.log("Contract deployed to:", funContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let funCount = await funContract.getTotalFuns();
+  
+  const funTxn = await funContract.Fun();
+  const funTxns = await funContract.Fun();
+
+  await funTxn.wait();
+  await funTxns.wait();
+
+  funCount = await funContract.getTotalFuns();
+
 };
 
 const runMain = async () => {
